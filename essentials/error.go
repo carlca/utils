ae0f4d7c19e8e2d93513d04c35eee396a2c6829e
carlca <carl.caulkett@gmail.com>
2017-12-12 15:04:12 +0000

    getting github up tp date

--- essentials/error.go
+++ essentials/error.go
@@ -8,8 +8,16 @@ import (
 )
 
 // CheckError is function to check error with call stack
-func CheckError(msg string, err error) {
-	if err != nil {
-		log.Fatal(fmt.Sprintf("%v: %+v", msg, errors.WithStack(err)))
+func CheckError(msg string, err error, both bool) {
+	if both {
+		if err == nil {
+			fmt.Println(fmt.Sprintf("%v succeeded", msg))
+		} else {
+			log.Fatal(fmt.Sprintf("%v failed: %+v", msg, errors.WithStack(err)))
+		}
+	} else {
+		if err != nil {
+			log.Fatal(fmt.Sprintf("%v failed: %+v", msg, errors.WithStack(err)))
+		}
 	}
 }
